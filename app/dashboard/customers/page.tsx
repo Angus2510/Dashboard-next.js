a41142e0cace66da6a191a0a1c7b2f54ae5033b7
Angus2510 <124569463+Angus2510@users.noreply.github.com>
2024-08-20 23:51:03 +0200

    set up the side bar and used pathname

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,5 @@
+const page = () => {
+  return <div>CUSTOMERS PAGE</div>;
+};
+
+export default page;
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,5 @@
+const page = () => {
+  return <div>DASHBOARD PAGE</div>;
+};
+
+export default page;
